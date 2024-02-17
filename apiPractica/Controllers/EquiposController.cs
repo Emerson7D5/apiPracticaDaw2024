92cd9212e3b2fff81dffb1e7546cc3bdb3dad8f4
Emerson7D5 <emerson.aguilar@gmail.com>
2024-02-17 07:34:17 -0600

    Add project files.

---
+++ apiPractica/Controllers/EquiposController.cs
@@ -0,0 +1,35 @@
+﻿using apiPractica.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiPractica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContext;
+
+        public EquiposController(equiposContext equiposContext)
+        {
+            _equiposContext = equiposContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipos = (from e in _equiposContext.equipos
+                                            select e).ToList();
+            
+            if (listadoEquipos.Count == 0) return NotFound();
+
+            return Ok(listadoEquipos);
+             
+        }
+
+
+
+
+    }
+}
